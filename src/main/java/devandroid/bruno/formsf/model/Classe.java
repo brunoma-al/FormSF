4d72283b3f4884a371025f9863adf5a00f9889bc
Bruno Gabriel <brugacag@gmail.com>
2023-11-16 22:56:41 -0300

    Finalizado.

--- src/main/java/devandroid/bruno/formsf/model/Classe.java
+++ src/main/java/devandroid/bruno/formsf/model/Classe.java
@@ -1,4 +1,71 @@
 package devandroid.bruno.formsf.model;
 
 public class Classe {
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String clienteEmail;
+    private String clienteEndereco;
+    private String clienteCandidatura;
+    private String clienteSenha;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getClienteEmail() {
+        return clienteEmail;
+    }
+
+    public void setClienteEmail(String clienteEmail) {
+        this.clienteEmail = clienteEmail;
+    }
+
+    public String getClienteEndereco() {
+        return clienteEndereco;
+    }
+
+    public void setClienteEndereco(String clienteEndereco) {
+        this.clienteEndereco = clienteEndereco;
+    }
+
+    public String getClienteCandidatura() {
+        return clienteCandidatura;
+    }
+
+    public void setClienteCandidatura(String clienteCandidatura) {
+        this.clienteCandidatura = clienteCandidatura;
+    }
+
+    public String getClienteSenha() {
+        return clienteSenha;
+    }
+
+    public void setClienteSenha(String clienteSenha) {
+        this.clienteSenha = clienteSenha;
+    }
+
+    @Override
+    public String toString() {
+        return "Classe{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", clienteEmail='" + clienteEmail + '\'' +
+                ", clienteEndereco='" + clienteEndereco + '\'' +
+                ", clienteCandidatura='" + clienteCandidatura + '\'' +
+                ", clienteSenha='" + clienteSenha + '\'' +
+                '}';
+    }
 }
--- src/main/java/devandroid/bruno/formsf/view/MainActivity.java
+++ src/main/java/devandroid/bruno/formsf/view/MainActivity.java
@@ -3,14 +3,104 @@ package devandroid.bruno.formsf.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.bruno.formsf.R;
+import devandroid.bruno.formsf.model.Classe;
 
 public class MainActivity extends AppCompatActivity {
+
+    Classe objeto;
+    Classe outroObjeto;
+    String dadosObjeto;
+    String dadosOutroObjeto;
+
+    EditText editNome;
+    EditText editSobrenome;
+    EditText editEmail;
+    EditText editEndereco;
+    EditText editCandidatura;
+    EditText editSenha;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        objeto = new Classe();
+        objeto.setPrimeiroNome("");
+        objeto.setSobreNome("");
+        objeto.setClienteEmail("");
+        objeto.setClienteEndereco("");
+        objeto.setClienteCandidatura("");
+        objeto.setClienteSenha("");
+
+        outroObjeto = new Classe();
+        outroObjeto.setPrimeiroNome("");
+        outroObjeto.setSobreNome("");
+        outroObjeto.setClienteEmail("");
+        outroObjeto.setClienteEndereco("");
+        outroObjeto.setClienteCandidatura("");
+        outroObjeto.setClienteSenha("");
+
+        editNome = findViewById(R.id.editNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editEmail = findViewById(R.id.editEmail);
+        editEndereco = findViewById(R.id.editEndereco);
+        editCandidatura = findViewById(R.id.editCandidatura);
+        editSenha = findViewById(R.id.editSenha);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        editNome.setText(objeto.getPrimeiroNome());
+        editSobrenome.setText(objeto.getSobreNome());
+        editEmail.setText(objeto.getClienteEmail());
+        editEndereco.setText(objeto.getClienteEndereco());
+        editCandidatura.setText(objeto.getClienteCandidatura());
+        editSenha.setText(objeto.getClienteSenha());
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editNome.setText("");
+                editSobrenome.setText("");
+                editEmail.setText("");
+                editEndereco.setText("");
+                editCandidatura.setText("");
+                editSenha.setText("");
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                objeto.setPrimeiroNome(editNome.getText().toString());
+                objeto.setSobreNome(editSobrenome.getText().toString());
+                objeto.setClienteEmail(editEmail.getText().toString());
+                objeto.setClienteEndereco(editEndereco.getText().toString());
+                objeto.setClienteCandidatura(editCandidatura.getText().toString());
+                objeto.setClienteSenha(editSenha.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo"+objeto.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Finalizado", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
